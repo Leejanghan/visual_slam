6bb43d8b138ce564808397f5694b514bfbed8b8d
이장한 <143863801+Leejanghan@users.noreply.github.com>
2024-09-10 21:39:54 +0900

    Update and rename test.py to test1.py

--- practice/test1.py
+++ practice/test1.py
@@ -1,8 +1,8 @@
 import cv2
 
-# opencv python 코딩 기본 틀
-# 카메라 영상을 받아올 객체 선언 및 설정(영상 소스, 해상도 설정)
-capture = cv2.VideoCapture(1)
+# basic frame of opencv 
+#카메라 영상을 받아올 객체 선언 및 설정(영상 소스, 해상도 설정)
+capture = cv2.VideoCapture(0)
 capture.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
@@ -11,9 +11,8 @@ while True:
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # 영상을 흑백으로 바꿔줌
 
     cv2.imshow("original", frame)   # frame(카메라 영상)을 original 이라는 창에 띄워줌 
-    if cv2.waitKey(1) == ord('q'):  # 키보드의 q 를 누르면 무한루프가 멈춤
-            break
-    
+    if cv2.waitKey(1) == ord('q'):  # 키보드의 q 를 누르면 무한루프가 멈춤 
+        break
 
-capture.release()                   # 캡처 객체를 없애줌
+capture.release()                   # 캡처 객체를 없애줌 // 장치 연결 종료 
 cv2.destroyAllWindows()             # 모든 영상 창을 닫아줌
