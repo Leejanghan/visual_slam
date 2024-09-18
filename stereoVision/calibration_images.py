d5345a69d311d793c8cbedbda2e7ebb6628b92da
이장한 <143863801+Leejanghan@users.noreply.github.com>
2024-09-18 20:05:49 +0900

    Update calibration_images.py

--- stereoVision/calibration_images.py
+++ stereoVision/calibration_images.py
@@ -1,29 +1,43 @@
 import cv2
+
 #스테레오비전 캘리브레이션을 위한 이미지 수집용 코드
-cap = cv2.VideoCapture(0)
-cap2 = cv2.VideoCapture(1)
+cap1 = cv2.VideoCapture(0)  # 1번 카메라
+cap2 = cv2.VideoCapture(1)  # 0번 카메라
+# In Linux --> cv2.CAP_V4L2 라는 default값을 추가하는 것이 좋음
 
 num = 0
 
-while cap.isOpened():
+# checking camera_1
+if not cap1.isOpened():
+    print('unable to read camera_1 feed')
 
-    succes1, img = cap.read()
-    succes2, img2 = cap2.read()
+# checking camera_2
+if not cap2.isOpened():
+    print('unable to read camera_2 feed')
+
+# recieve image data
+while cap1.isOpened() and cap2.isOpened():
+    success1, img1 = cap1.read()
+    success2, img2 = cap2.read()
 
     k = cv2.waitKey(5)
 
+    # Mean Esc
     if k == 27:
         break
-    elif k == ord('s'): # wait for 's' key to save and exit
-        cv2.imwrite('images/stereoLeft/imageL' + str(num) + '.png', img)
+    # Wait for 's' key to save and exit
+    elif k == ord('s'):
+        cv2.imwrite('images/stereoLeft/imageL' + str(num) + '.png', img1)
         cv2.imwrite('images/stereoRight/imageR' + str(num) + '.png', img2)
         print("images saved!")
         num += 1
 
-    cv2.imshow('Img 1',img)
+    cv2.imshow('Img 1',img1)
     cv2.imshow('Img 2',img2)
 
-cap.release()
+cap1.release()
 cap2.release()
 
 cv2.destroyAllWindows()
+
+### 시간 동기화가 잘 이루어지지 않는다면, 목표 프레임 레이트를 설정해야할 수 있음
