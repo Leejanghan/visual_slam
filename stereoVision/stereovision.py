9dd294b9d244788f99ad83e8dbf76aa5ec38c414
이장한 <143863801+Leejanghan@users.noreply.github.com>
2024-09-22 21:32:18 +0900

    Update stereovision.py

--- stereoVision/stereovision.py
+++ stereoVision/stereovision.py
@@ -1,7 +1,23 @@
+# Basic module
 import cv2
 import numpy as np
+# Use to check distance
 import matplotlib
 import matplotlib.pyplot as plt
+
+# To check depth (Need to change disp type)
+""" def coords_mouse_disp(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDBLCLK:
+        # print x,y,disp[y,x],filteredImg[y,x]
+        average = 0
+        for u in range(-1, 2):
+            for v in range(-1, 2):
+                average += disp[y + u, x + v]
+        average = average / 9
+        Distance = -593.97 * average ** (3) + 1506.8 * average ** (2) - 1373.1 * average + 522.06
+        Distance = np.around(Distance * 0.01, decimals=2)
+        print('Distance: ' + str(Distance) + ' m')
+"""
 
 # Camera parameters to undistort and rectify images
 cv_file = cv2.FileStorage()
@@ -21,9 +37,9 @@ cap_right = cv2.VideoCapture(1)   # 0번 카메라, RIGHT
 kernel = np.ones((3, 3), np.uint8)
 
 # Prepare parameter
-nDispFactor = 5  # adjust this
-window_size = 5
-min_disp = 2
+nDispFactor = 8  # adjust this
+window_size = 12
+min_disp = 5  # 가까운 물체를 인식 안하는 방향 --> 5가 최대 적정 값일듯?
 
 # Create SGBM object
 stereo = cv2.StereoSGBM_create(
@@ -32,7 +48,7 @@ stereo = cv2.StereoSGBM_create(
     blockSize = window_size, # 매칭 블록 크기 설정
     P1=8 * 3 * window_size ** 2, # setting smoothness
     P2=32 * 3 * window_size ** 2,
-    disp12MaxDiff = 5, # 두 시차 맵 간의 최대 허용 차이 설정
+    disp12MaxDiff = 3, # 두 시차 맵 간의 최대 허용 차이 설정
     uniquenessRatio = 10, # 값이 클수록 매칭 난이도 올라감
     speckleWindowSize = 100, # 작은 노이즈 영역을 제거하기 위함
     speckleRange = 32
@@ -45,11 +61,12 @@ stereo = cv2.StereoSGBM_create(
 # lmbda = 80000
 # sigma = 1.8
 # visual_multiplier = 1.0
+# 만약, 프레임이 너무 떨어진다면 위 파라미터값을 조정해야함
 
 # wls_filter = cv2.ximgproc.createDisparityWLSFilter(matcher_left=stereo)
 # wls_filter.setLambda(lmbda)
 # wls_filter.setSigmaColor(sigma)
-# 연산량이 너무 많아 프레임이 떨어짐
+# wls filter <-- 연산량이 너무 많아 프레임이 떨어짐
 
 # Starting the stereo vision
 while(cap_right.isOpened() and cap_left.isOpened()):
@@ -63,8 +80,8 @@ while(cap_right.isOpened() and cap_left.isOpened()):
     grayL = cv2.cvtColor(frame_left, cv2.COLOR_BGR2GRAY)
 
     # 조명 환경이 불균형일 때
-    grayR = cv2.equalizeHist(grayR) # 카메라 노출도 차이 히스토그램 평활화를 통해 보완
-    grayL = cv2.equalizeHist(grayL)
+    # grayR = cv2.equalizeHist(grayR) # 카메라 노출도 차이 히스토그램 평활화를 통해 보완
+    # grayL = cv2.equalizeHist(grayL)
 
     # 합쳐진 형상이 다음과 같음
     blended = cv2.addWeighted(grayL,0.5,grayR,0.5,0)
@@ -87,14 +104,14 @@ while(cap_right.isOpened() and cap_left.isOpened()):
     disp8 = cv2.normalize(disparity, None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8U)
     disp_filtered = cv2.medianBlur(disp8, ksize=5)
 
-    # 3. Morphological Filter 적용
+    # Morphological Filter 적용
     closing = cv2.morphologyEx(disp_filtered, cv2.MORPH_CLOSE, kernel)
 
-    disp_colored = cv2.applyColorMap(disp_filtered, cv2.COLORMAP_JET)
+    disp_colored = cv2.applyColorMap(closing, cv2.COLORMAP_OCEAN)
 
     cv2.imshow("calibration_cap_right", frame_right)
     cv2.imshow("calibration_cap_left", frame_left)
-    cv2.imshow('disparity_map', closing)
+    cv2.imshow('disparity_map', disp_colored)
     cv2.imshow('blended', blended)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
